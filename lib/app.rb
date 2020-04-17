9a4f6db28bbbfa7d2ea35760ac7c332ef8e24456
Snowballe <snacki3andmr.5@gmail.com>
2020-04-17 10:10:09 +0200

    opla

--- lib/app.rb
+++ lib/app.rb
@@ -4,12 +4,15 @@ require 'open-uri'
 Dotenv.load
 
 page=Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
 all_devise_links=[]
 all_price_links=[]
 final_array=[]
+
 for i in 1..200
-all_devise_links<<page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[3]/div").text
-all_price_links<<page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[5]/a").text
-final_array<<all_devise_links.zip(all_price_links)
+    all_devise_links<<page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[3]/div").text
+    all_price_links<<page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[5]/a").text
+    final_array<<all_devise_links.zip(all_price_links)
 end
+
 puts final_array
